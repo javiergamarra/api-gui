9fd71131fb5a32bf2051a24f4e39a82e097c2ef5
Javier Gamarra <nhpatt@gmail.com>
2020-04-08 12:59:04 +0200

    Render component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
 
+import './styles.css';
 import App from './App';
 
-export default function main({portletNamespace, contextPath, portletElementId, configuration}) {
-	ReactDOM.render(
-		<App 
-			portletNamespace={portletNamespace}
-			contextPath={contextPath}
-			portletElementId={portletElementId}
-			configuration={configuration}
-		/>,
-		document.getElementById(portletElementId)
-	);
-}
+ReactDOM.render(<App/>, document.getElementById('root'));
